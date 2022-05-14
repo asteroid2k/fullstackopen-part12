39471edfe88d031893977cdeba63fdbdb41becca
Kwaku Adusei Okyere <dobridobrev900@gmail.com>
2022-05-14 15:50:44 +0000

    12.11

--- todo-app/todo-backend/routes/todos.js
+++ todo-app/todo-backend/routes/todos.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const { Todo } = require("../mongo");
+const redis = require("../redis");
+
 const router = express.Router();
 
 /* GET todos listing. */
@@ -14,6 +16,8 @@ router.post("/", async (req, res) => {
     text: req.body.text,
     done: false,
   });
+  let count = (await redis.getAsync("added_todos")) || 0;
+  await redis.setAsync("added_todos", Number(count) + 1);
   res.send(todo);
 });
 
